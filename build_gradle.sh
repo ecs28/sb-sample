eee520b8a2d4185447852cfe0ac08aff839da393
Eduardo Cardoso <eduardo@Eduardos-MacBook-Pro.local>
2018-06-19 11:27:19 +0100

    bla

--- build_gradle.sh
+++ build_gradle.sh
@@ -2,7 +2,15 @@
 set +x
 cd sb-sample/
 sh gradlew --no-daemon clean build
-mkdir -p ../dist
-cd ..
-mv sb-sample/build/libs/* ./dist/
-ls ./dist/
+if [ $? -eq 0 ]
+then
+  mkdir -p ../dist
+  cd ..
+  mv sb-sample/build/libs/* ./dist/ 
+   ls ./dist/
+  exit 0
+else
+  echo "The script failed" >&2
+  exit 1
+fi
+
